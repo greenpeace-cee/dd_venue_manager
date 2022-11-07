da5d1245639794aba6884247b48ebcc7df235358
Patrick Figel <pfigel@greenpeace.org>
2022-11-07 14:13:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# dd_venue_manager
+
+![Screenshot](/images/screenshot.png)
+
+(*FIXME: In one or two paragraphs, describe what the extension does and why one would download it. *)
+
+The extension is licensed under [AGPL-3.0](LICENSE.txt).
+
+## Requirements
+
+* PHP v7.4+
+* CiviCRM (*FIXME: Version number*)
+
+## Installation (Web UI)
+
+Learn more about installing CiviCRM extensions in the [CiviCRM Sysadmin Guide](https://docs.civicrm.org/sysadmin/en/latest/customize/extensions/).
+
+## Installation (CLI, Zip)
+
+Sysadmins and developers may download the `.zip` file for this extension and
+install it with the command-line tool [cv](https://github.com/civicrm/cv).
+
+```bash
+cd <extension-dir>
+cv dl dd_venue_manager@https://github.com/FIXME/dd_venue_manager/archive/master.zip
+```
+or
+```bash
+cd <extension-dir>
+cv dl dd_venue_manager@https://lab.civicrm.org/extensions/dd_venue_manager/-/archive/main/dd_venue_manager-main.zip
+```
+
+## Installation (CLI, Git)
+
+Sysadmins and developers may clone the [Git](https://en.wikipedia.org/wiki/Git) repo for this extension and
+install it with the command-line tool [cv](https://github.com/civicrm/cv).
+
+```bash
+git clone https://github.com/FIXME/dd_venue_manager.git
+cv en dd_venue_manager
+```
+or
+```bash
+git clone https://lab.civicrm.org/extensions/dd_venue_manager.git
+cv en dd_venue_manager
+```
+
+## Getting Started
+
+(* FIXME: Where would a new user navigate to get started? What changes would they see? *)
+
+## Known Issues
+
+(* FIXME *)
---
+++ dd_venue_manager.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'dd_venue_manager.civix.php';
+// phpcs:disable
+use CRM_DdVenueManager_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function dd_venue_manager_civicrm_config(&$config): void {
+  _dd_venue_manager_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function dd_venue_manager_civicrm_install(): void {
+  _dd_venue_manager_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function dd_venue_manager_civicrm_postInstall(): void {
+  _dd_venue_manager_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function dd_venue_manager_civicrm_uninstall(): void {
+  _dd_venue_manager_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function dd_venue_manager_civicrm_enable(): void {
+  _dd_venue_manager_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function dd_venue_manager_civicrm_disable(): void {
+  _dd_venue_manager_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function dd_venue_manager_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _dd_venue_manager_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function dd_venue_manager_civicrm_entityTypes(&$entityTypes): void {
+  _dd_venue_manager_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function dd_venue_manager_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function dd_venue_manager_civicrm_navigationMenu(&$menu): void {
+//  _dd_venue_manager_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _dd_venue_manager_civix_navigationMenu($menu);
+//}
