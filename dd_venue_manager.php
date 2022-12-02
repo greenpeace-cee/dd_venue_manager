b90e5ab713c29ce15f71e5eb8d406be17d05911e
Agiliway Group, Inc <github@agiliway.com>
2022-12-02 11:20:43 +0200

    GP-35450: Remove examples.

--- dd_venue_manager.php
+++ dd_venue_manager.php
@@ -37,31 +37,3 @@ function dd_venue_manager_civicrm_links($operation, $objectName, $objectId, &$li
     ];
   }
 }
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
- */
-//function dd_venue_manager_civicrm_preProcess($formName, &$form): void {
-//
-//}
-
-/**
- * Implements hook_civicrm_navigationMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
- */
-//function dd_venue_manager_civicrm_navigationMenu(&$menu): void {
-//  _dd_venue_manager_civix_insert_navigation_menu($menu, 'Mailings', [
-//    'label' => E::ts('New subliminal message'),
-//    'name' => 'mailing_subliminal_message',
-//    'url' => 'civicrm/mailing/subliminal',
-//    'permission' => 'access CiviMail',
-//    'operator' => 'OR',
-//    'separator' => 0,
-//  ]);
-//  _dd_venue_manager_civix_navigationMenu($menu);
-//}
