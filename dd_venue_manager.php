999d7ff72aa7bfa8ac54983196db37fdbf332706
Agiliway Group, Inc <github@agiliway.com>
2022-12-02 11:20:17 +0200

    GP-35450: Add Upgrader. Remove install hooks.

--- dd_venue_manager.php
+++ dd_venue_manager.php
@@ -15,60 +15,6 @@ function dd_venue_manager_civicrm_config(&$config): void {
 }
 
 /**
- * Implements hook_civicrm_install().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
- */
-function dd_venue_manager_civicrm_install(): void {
-  _dd_venue_manager_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function dd_venue_manager_civicrm_postInstall(): void {
-  _dd_venue_manager_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function dd_venue_manager_civicrm_uninstall(): void {
-  _dd_venue_manager_civix_civicrm_uninstall();
-}
-
-/**
- * Implements hook_civicrm_enable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
- */
-function dd_venue_manager_civicrm_enable(): void {
-  _dd_venue_manager_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function dd_venue_manager_civicrm_disable(): void {
-  _dd_venue_manager_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function dd_venue_manager_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _dd_venue_manager_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
