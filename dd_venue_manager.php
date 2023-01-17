62ed4fd75e3ea027f7c83e77f79ece2fa60c977e
Patrick Figel <pfigel@greenpeace.org>
2023-01-17 11:53:27 +0100

    GP-37735 Add contact person case summary demo

--- dd_venue_manager.php
+++ dd_venue_manager.php
@@ -37,3 +37,44 @@ function dd_venue_manager_civicrm_links($operation, $objectName, $objectId, &$li
     ];
   }
 }
+
+function dd_venue_manager_civicrm_caseSummary($caseID) {
+  // fetch e.g. using:
+  /*
+   * $relationships = \Civi\Api4\Relationship::get()
+   *   ->addWhere('relationship_type_id:name', '=', 'venue contact person')
+   *   ->addWhere('case_id', '=', $caseID)
+   *   ->execute();
+   */
+  return [
+    'relationships' => [
+      'label' => '<h4>Contact Persons</h4>',
+      'value' => '
+<table id="caseRoles-selector-1" class="dataTable no-footer" role="grid" aria-describedby="caseRoles-selector-1_info">
+        <thead>
+          <tr role="row">
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Primary?</th>
+            <th>Position</th>
+            <th>Decision Maker?</th>
+            <th>DDC Disposition</th>
+            <th>On-Site Contact Person?</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+      <tbody><tr role="row" class="odd"><td><a class="view-contact" title="View Contact" href="#">Patrick Figel</a></td>
+      <td>+43 123 4567890</td>
+      <td>pfigel@greenpeace.org</td>
+      <td>Yes</td>
+      <td>Filialleiter</td>
+      <td>No</td>
+      <td>pro DDC</td>
+      <td>Yes</td>
+      <td><a href="/civicrm/contact/view/rel?action=update&reset=1&cid=203&id=218&rtype=a_b" title="Edit Contact Person"><i class="crm-i fa-pencil" aria-hidden="true"></i></a><a href="#deleteCaseRoleDialog" title="Remove Contact Person" class="crm-hover-button case-miniform"><span class="icon delete-icon"></span></a></td></tr></tbody></table>
+',
+    ],
+  ];
+}
+
