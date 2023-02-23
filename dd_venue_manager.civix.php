ee33461b631b7ceb5b7170b9c0617602fe835c46
Patrick Figel <pfigel@greenpeace.org>
2023-02-23 06:59:35 +0100

    Update civix, add dependencies

--- dd_venue_manager.civix.php
+++ dd_venue_manager.civix.php
@@ -108,6 +108,103 @@ function _dd_venue_manager_civix_civicrm_config(&$config = NULL) {
 }
 
 /**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function _dd_venue_manager_civix_civicrm_install() {
+  _dd_venue_manager_civix_civicrm_config();
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    $upgrader->onInstall();
+  }
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function _dd_venue_manager_civix_civicrm_postInstall() {
+  _dd_venue_manager_civix_civicrm_config();
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onPostInstall'])) {
+      $upgrader->onPostInstall();
+    }
+  }
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function _dd_venue_manager_civix_civicrm_uninstall(): void {
+  _dd_venue_manager_civix_civicrm_config();
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    $upgrader->onUninstall();
+  }
+}
+
+/**
+ * (Delegated) Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function _dd_venue_manager_civix_civicrm_enable(): void {
+  _dd_venue_manager_civix_civicrm_config();
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onEnable'])) {
+      $upgrader->onEnable();
+    }
+  }
+}
+
+/**
+ * (Delegated) Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ * @return mixed
+ */
+function _dd_venue_manager_civix_civicrm_disable(): void {
+  _dd_venue_manager_civix_civicrm_config();
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onDisable'])) {
+      $upgrader->onDisable();
+    }
+  }
+}
+
+/**
+ * (Delegated) Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *   for 'enqueue', returns void
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function _dd_venue_manager_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  if ($upgrader = _dd_venue_manager_civix_upgrader()) {
+    return $upgrader->onUpgrade($op, $queue);
+  }
+}
+
+/**
+ * @return CRM_DdVenueManager_Upgrader
+ */
+function _dd_venue_manager_civix_upgrader() {
+  if (!file_exists(__DIR__ . '/CRM/DdVenueManager/Upgrader.php')) {
+    return NULL;
+  }
+  else {
+    return CRM_DdVenueManager_Upgrader_Base::instance();
+  }
+}
+
+/**
  * Inserts a navigation menu item at a given place in the hierarchy.
  *
  * @param array $menu - menu hierarchy
